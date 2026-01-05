4842be6a379256ee5bbda0a14487e4c85b8d1724
Aman Chaurasiya <139747366+aman8906@users.noreply.github.com>
2026-01-05 22:14:55 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -8,7 +8,7 @@ const generateToken = (res, payload) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
+    sameSite: "none",
     maxAge: 24 * 60 * 60 * 1000,
   });
   return token;
